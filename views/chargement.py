5062fa7fa4222d28818fc16db98cac672a7a8f41
yassinerazine-create <yassine.razine@gmail.com>
2026-03-06 17:43:51 +0100

    maj chargement données

--- views/chargement.py
+++ views/chargement.py
@@ -6,10 +6,23 @@ def show():
 
     file = st.file_uploader("Importer fichier", type=["csv","xlsx"])
     if file:
+
+        #lecture du fichier
         if file.name.endswith("csv"):
             df = pd.read_csv(file)
         else:
             df = pd.read_excel(file)
+
+
+        #Nettoyage du fichier
+        if "RFC" in df.columns:
+            #garder uniquement les lignes avec donnée RFC commenant par 'RFC'
+            df=df[df["RFC"].astype(str).str.upper().str.startswith("RFC")]
+
+            st.success ("donnée nettoyées : lignes sans RFC ou RFC incorrectes supprimées")
+
+        else:
+            st.warning("Colonne 'RFC' non trouvée dans le fichier")
 
         st.session_state["data"] = df
         st.success("Données chargées")
