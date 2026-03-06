2f1d0aa176d365dbd7302f16b82f6f890c6f37d6
yassinerazine-create <yassine.razine@gmail.com>
2026-03-06 18:32:23 +0100

    clean

--- utils/clean.py
+++ utils/clean.py
@@ -5,7 +5,7 @@ def filter_columns(df):
     """
     Garde uniquement les colonnes essentielles
     """
-    cols = ["RFC", "UMEP", "Semaine cible", "Label Livraison affecté"]
+    cols = ["Composant_Version", "RFC", "UMEP", "Semaine cible", "Label Livraison affecté"]
     df = df[[c for c in cols if c in df.columns]]
     df = split_composant_version(df)
     return df
