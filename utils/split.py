71da84021bcd8ae80fb789ee93e15de4c773c2d3
yassinerazine-create <yassine.razine@gmail.com>
2026-03-06 18:38:25 +0100

    split amelioré (ECHANGES)

--- utils/split.py
+++ utils/split.py
@@ -2,45 +2,40 @@ import pandas as pd
 
 def split_composant_version(df):
     """
-    Scinde la colonne Composant_Version en deux colonnes : Composant et Version
-    Règles :
-    - ECHANGES_025.149.01:ECHANGES_025.149.01  -> Composant=ECHANGES, Version=025.149.01
-    - ECHANGES_025.110:02                       -> Composant=ECHANGES, Version=025.110:02
-    - Les autres : split sur ':' directement
+    Scinde la colonne Composant_Version en Composant et Version
+    Cas traités :
+    - abcf_sla_101_interface_fournisseurs:2.00.27 -> Composant = abcf_sla_101_interface_fournisseurs, Version = 2.00.27
+    - ECHANGES_025.110:02 -> Composant = ECHANGES, Version = 025.110:02
+    - ECHANGES_025.149.01:ECHANGES_025.149.01 -> Composant = ECHANGES, Version = 025.149.01
     """
 
     def extract(val):
         val = str(val)
 
-        # Cas ECHANGES spécial
         if val.startswith("ECHANGES_"):
-            # si format ECHANGES_025.149.01:ECHANGES_025.149.01
-            if ':' in val:
-                parts = val.split(':')
-                composant_part = parts[0]
-                version_part = parts[1]
+            # Composant = ECHANGES
+            composant = "ECHANGES"
 
-                # Composant = partie avant premier '_'
-                composant = composant_part.split('_')[0]
-                # Version = partie après premier '_' de la première partie
-                version = composant_part[len(composant)+1:]
-                
-                # Mais si la partie après ':' est dupliquée comme ECHANGES_025.149.01, on prend juste la version
-                # pour qu'on ait 025.149.01
-                if version.startswith(composant + "_"):
-                    version = version[len(composant)+1:]
-                
-                # Si format comme ECHANGES_025.110:02, version = 025.110:02
-                if version_part != composant_part:
-                    version = version_part
-            else:
-                # Pas de ':', split sur '_' pour composant et version
-                composant = val.split('_')[0]
-                version = val[len(composant)+1:]
-        else:
-            # Autres composants : split sur ':'
+            # Vérifier s'il y a ':'
             if ':' in val:
-                composant, version = val.split(':', 1)
+                parts = val.split(':', 1)
+                left = parts[0]   # ECHANGES_025.110 ou ECHANGES_025.149.01
+                right = parts[1]  # peut être "02" ou "ECHANGES_025.149.01"
+
+                # Pour ECHANGES_025.149.01:ECHANGES_025.149.01
+                if right.startswith("ECHANGES_"):
+                    version = left.split('_',1)[1]  # tout après le premier '_'
+                else:
+                    # Pour ECHANGES_025.110:02
+                    version = left.split('_',1)[1] + ":" + right  # 025.110:02
+            else:
+                # Pas de ':', juste prendre après premier '_'
+                version = val.split('_',1)[1]
+
+        else:
+            # Autres composants : split normal sur ':'
+            if ':' in val:
+                composant, version = val.split(':',1)
             else:
                 composant = val
                 version = ''
