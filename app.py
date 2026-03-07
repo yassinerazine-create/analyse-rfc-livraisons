c1624128dc9990adad34907600f2e980d71fb374
yassinerazine-create <yassine.razine@gmail.com>
2026-03-07 08:53:13 +0100

    maj

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import views
-import style
+from views import chargement, vue_ensemble, livraisons, rfc, composants, umep, incoherences
+from style import apply_style
 
 st.set_page_config(
     page_title="Analyse RFC Livraisons",
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
-style.apply_style()
+apply_style()
 
 st.sidebar.title("📦 Analyse RFC")
 
@@ -26,7 +26,7 @@ menu = st.sidebar.radio(
 )
 
 if menu == "Chargement":
-    views.chargement.show()
+    chargement.show()
 
 else:
 
@@ -37,19 +37,19 @@ else:
     df = st.session_state.df
 
     if menu == "Vue d'ensemble":
-        views.vue_ensemble.show(df)
+        vue_ensemble.show(df)
 
     if menu == "Livraisons":
-        views.livraisons.show(df)
+        livraisons.show(df)
 
     if menu == "RFC":
-        views.rfc.show(df)
+        rfc.show(df)
 
     if menu == "Composants":
-        views.composants.show(df)
+        composants.show(df)
 
     if menu == "UMEP":
-        views.umep.show(df)
+        umep.show(df)
 
     if menu == "Incohérences":
-        views.incoherences.show(df)
+        incoherences.show(df)
